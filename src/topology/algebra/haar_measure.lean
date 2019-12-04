88cc00b14a1bf1e1c829d109dfc594fa6800f54f
Johan Commelin <johan@commelin.net>
2019-12-04 11:35:41 +0100

    Small start

---
+++ src/topology/algebra/haar_measure.lean
@@ -0,0 +1,41 @@
+/-
+Copyright (c) 2019 Johan Commelin. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Johan Commelin
+-/
+
+import topology.algebra.group
+import measure_theory.borel_space
+import measure_theory.measure_space
+import tactic
+
+/-!
+# Haar measure
+
+In this file we define the Haar measure:
+A left-translation invariant measure on a locally compact topological group.
+
+## References
+Alfsen, E. M. A simplified constructive proof of the existence and uniqueness of Haar measure.
+  Math. Scand. 12 (1963), 106--116. MR0158022
+
+-/
+
+variables {G : Type*}
+variables [group G] [topological_space G] [topological_group G] [locally_compact_space G]
+
+namespace measure_theory
+
+def left_translate (μ : measure G) (g : G) : measure G :=
+{ measure_of := λ s, μ ((*) g '' s),
+  empty := by { suffices : (*) g '' ∅ = ∅, { rw [this, measure_empty] }, ext, simp, },
+  mono := λ s₁ s₂ h, measure_mono $ by { rintro _ ⟨x, hx, rfl⟩, exact ⟨_, h hx, rfl⟩ },
+  Union_nat := λ f,
+  begin
+    library_search
+  end,
+  m_Union := sorry,
+  trimmed := sorry }
+
+
+end measure_theory
